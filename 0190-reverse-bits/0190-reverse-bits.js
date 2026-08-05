c076b3778096476d3362930883c3c309d1de5326
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-08-05 15:33:06 +0530

    Time: 41 ms (85.09%), Space: 54.4 MB (43.18%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverseBits = function(n) {
+    let left = 0;
+    let right = 31;
+    while(left < right)
+    {
+        let leftbit = (n >>> left)&1;
+        var rightbit = (n >>> right)&1;
+        if(leftbit != rightbit)
+        {
+             n ^= (1 << left);
+             n ^= (1 << right);
+        }
+        left++;
+        right--;
+    }
+    return n>>>0
+};
