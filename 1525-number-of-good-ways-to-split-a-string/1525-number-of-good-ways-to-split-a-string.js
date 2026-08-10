b4d026ff79d4034dfc3eb90251930f460f21b880
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-08-10 06:51:46 +0530

    Time: 27 ms (62.5%), Space: 58.6 MB (50%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.js
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.js
@@ -6,27 +6,20 @@ var numSplits = function(s) {
     let ans = 0;
     let leftSet = new Set();
     let rightSet = new Set();
-    let freqMap = new Map();
+    let freqMap = new Array(26).fill(0)
     for(const ch of s)
     {
         rightSet.add(ch);
     }
     for(const ch of s)
     {
-        if(freqMap.has(ch))
-        {
-            freqMap.set(ch,freqMap.get(ch)+1)
-        }
-        else
-        {
-            freqMap.set(ch,1);
-        }
+       freqMap[ch.charCodeAt(0)-97]++;
     }
     for(let right = 0; right < s.length-1;right++)
     {
         leftSet.add(s[right])
-        freqMap.set(s[right],freqMap.get(s[right])-1);
-        if(freqMap.get(s[right]) == 0)
+        freqMap[s[right].charCodeAt(0)-97]--;
+        if(freqMap[s[right].charCodeAt(0)-97] == 0)
         rightSet.delete(s[right]);
         if(leftSet.size == rightSet.size)
         ans++;
