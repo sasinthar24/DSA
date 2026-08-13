a1119910562900a34be6d66b36247cca9190da14
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-08-13 16:11:51 +0530

    Time: 1 ms (96.96%), Space: 57.9 MB (44.05%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -7,6 +7,11 @@ var nextGreaterElement = function(nums1, nums2) {
     let stack = [];
     const n = nums2.length;
     let nextGreater = new Array(n).fill(-1);
+     let map = new Map();
+    for(let i = 0; i < nums2.length;i++)
+    {
+          map.set(nums2[i],i);
+    }
     for(let i = n-1; i>= 0; i--)
     {
         while(stack.length > 0 && nums2[i] > stack[stack.length-1])
@@ -26,7 +31,7 @@ var nextGreaterElement = function(nums1, nums2) {
     let result = [];
     for(let i = 0; i < nums1.length;i++)
     {
-        let idx = nums2.indexOf(nums1[i]);
+        let idx = map.get(nums1[i]);
         result.push(nextGreater[idx]);
     }
     return result;
