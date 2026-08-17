e046a523dc7d9681841fda09642fc7c66267de76
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-08-17 16:03:07 +0530

    Time: 0 ms (100%), Space: 54.1 MB (29.69%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+  const n = nums.length;
+  if(n === 1)
+  return 0;
+  if(nums[0] > nums[1])
+  return 0;
+  if(nums[n-1] > nums[n-2])
+  return n-1;
+
+  let low = 1;
+  let high = nums.length-2
+  while(low <= high)
+  {
+    let mid = Math.floor((low+high)/2);
+    if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1])
+    return mid;
+
+    if(nums[mid] > nums[mid-1])
+    low = mid+1;
+    else
+    high = mid-1;
+  }
+  return -1;
+};
