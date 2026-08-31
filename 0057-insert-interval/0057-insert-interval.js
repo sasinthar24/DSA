0ea35fbb44b70aef3ccf988a907b8ba9b983abf7
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-08-31 20:17:50 +0530

    Time: 3 ms (59.84%), Space: 58 MB (73.59%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,24 +4,37 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    intervals.push(newInterval);
-    intervals.sort((a,b)=> a[0] - b[0]);
-
-    let start = intervals[0][0];
-    let end = intervals[0][1];
-    let ans = [];
-    for(let i = 1; i < intervals.length;i++)
+    let newArray = [];
+    let inserted = false;
+    for(let i = 0;i < intervals.length;i++)
     {
-       if(end >= intervals[i][0])
+        if(!inserted && newInterval[0] <= intervals[i][0])
+        {
+            newArray.push(newInterval)
+            inserted = true;
+        }
+
+        newArray.push(intervals[i])
+    }
+    if(!inserted)
+    newArray.push(newInterval)
+   
+
+    let start = newArray[0][0];
+    let end = newArray[0][1];
+    let ans = [];
+    for(let i = 1; i < newArray.length;i++)
+    {
+       if(end >= newArray[i][0])
        {
-        start = Math.min(start,intervals[i][0])
-        end = Math.max(end,intervals[i][1])
+        start = Math.min(start,newArray[i][0])
+        end = Math.max(end,newArray[i][1])
        }
        else
        {
         ans.push([start,end])
-        start = intervals[i][0]
-        end = intervals[i][1]
+        start = newArray[i][0]
+        end = newArray[i][1]
        }
     }
     ans.push([start,end])
