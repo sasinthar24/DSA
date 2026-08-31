81fa46b42b3118ca332339278b6b52f57fad4a20
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-08-31 23:08:39 +0530

    Time: 79 ms (75.71%), Space: 79.5 MB (46.33%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -7,7 +7,7 @@ var findMinArrowShots = function(points) {
 
     let start = points[0][0];
     let end = points[0][1];
-    let ans = [];
+    let ans = 1;
     for(let i = 1; i < points.length;i++)
     {
         if(end >= points[i][0])
@@ -17,11 +17,11 @@ var findMinArrowShots = function(points) {
         }
         else
         {
-             ans.push([start,end]);
+            ans++
             start = points[i][0]
             end = points[i][1]
         }
     }
-    ans.push([start,end])
-    return ans.length
+
+    return ans
 };
