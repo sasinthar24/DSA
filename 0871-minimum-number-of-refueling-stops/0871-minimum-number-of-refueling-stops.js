279229e2ff69a9084362d97f40dd6aac33c163ac
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-03 21:45:50 +0530

    Time: 21 ms (18.18%), Space: 62.1 MB (30.91%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.js
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.js
@@ -4,25 +4,103 @@
  * @param {number[][]} stations
  * @return {number}
  */
-var minRefuelStops = function(target, startFuel, stations) {
-    const n = stations.length
-    const dp = new Array(n+1).fill(0);
-    dp[0] = startFuel;
-    for(let i = 0 ;i < n;i++)
+ class heap
+{
+    constructor(compare)
     {
-        const [position,fuel] = stations[i]
-        for(let stops = i; stops>=0;stops--)
+        this.heap = [];
+        this.compare = compare;
+    }
+
+    size()
+    {
+        return this.heap.length
+    }
+    peek()
+    {
+        if(this.heap.length == 0)
+        return null
+        return this.heap[0]
+    }
+    push(val)
+    {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+    bubbleUp()
+    {
+        let index = this.heap.length-1
+        while(index >0)
         {
-            if(dp[stops] >= position)
-            {
-                dp[stops+1] = Math.max(dp[stops+1], dp[stops]+fuel);
-            }
+            let parent = Math.floor((index-1)/2)
+            if(this.compare(this.heap[parent],this.heap[index]))
+            break;
+            [this.heap[parent],this.heap[index]] = [this.heap[index],this.heap[parent]];
+            index = parent
         }
     }
-    for(let i = 0; i < dp.length;i++)
+
+    pop()
     {
-        if(dp[i] >= target)
-        return i
+        if(this.heap.length == 0)
+        return null
+        if(this.heap.length == 1)
+        return this.heap.pop()
+        let top = this.heap[0]
+        this.heap[0] = this.heap.pop()
+        this.bubbleDown()
+        return top
     }
-    return -1
+    bubbleDown()
+    {
+        let index = 0;
+        let n = this.heap.length
+        while(true)
+        {
+            let left = 2 * index +1;
+            let right = 2 * index +2;
+            let best = index;
+            if(left < n&&!this.compare(this.heap[best],this.heap[left]))
+            {
+                best = left
+            }
+            if(right < n &&!this.compare(this.heap[best],this.heap[right]))
+            {
+                best = right
+            }
+            if(index == best)
+            break;
+            [this.heap[best],this.heap[index]] = [this.heap[index],this.heap[best]]
+            index = best;
+        }
+    }
+}
+var minRefuelStops = function(target, startFuel, stations) {
+  let prev = 0;
+  let stop = 0;
+  let fuel = startFuel;
+  let maxHeap = new heap((a,b)=> a >= b);
+  for(let i = 0; i < stations.length; i++)
+  {
+    const [currentPosition,currentFuel] = stations[i];
+    fuel-= currentPosition - prev;
+    while(fuel < 0 && maxHeap.size() > 0)
+    {
+        fuel+=maxHeap.pop();
+        stop++;
+    }
+    if(fuel < 0 )
+    return -1;
+
+    maxHeap.push(currentFuel);
+    prev = currentPosition;
+  }
+  fuel-= target-prev
+  while(fuel < 0 && maxHeap.size()>0)
+  {
+    fuel+=maxHeap.pop();
+    stop++;
+  }
+  return fuel >= 0 ? stop : -1;
+
 };
