794610fa0b692ca93d47a29276c24da83ba398ec
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-05 07:01:30 +0530

    Time: 373 ms (77.55%), Space: 186 MB (20.41%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[][]} isWater
+ * @return {number[][]}
+ */
+var highestPeak = function(isWater) {
+    const n = isWater.length;
+    const m = isWater[0].length;
+    let grid = Array.from({length:n},()=> new Array(m).fill(-1));
+
+    const dx = [0,1,-1,0]
+    const dy = [1,0,0,-1]
+
+    let queue = [];
+    let head = 0;
+    for(let r = 0; r < n; r++)
+    {
+        for(let c = 0; c < m ; c++)
+        {
+            if(isWater[r][c] == 1)
+            {
+                grid[r][c] = 0;
+                queue.push([r,c])
+            }
+        }
+    }
+    while(head < queue.length)
+    {
+        const [r,c] = queue[head++]
+        for(let k = 0; k < 4; k++ )
+        {
+            let nr = r + dx[k];
+            let nc = c + dy[k];
+
+            if(nr >=0 && nr < n && nc >=0 && nc < m && grid[nr][nc] == -1)
+            {
+                grid[nr][nc] = grid[r][c]+1;
+                queue.push([nr,nc])
+            }
+        }
+    }
+    return grid
+};
