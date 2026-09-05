69f733baaad2ca66f87f8e5954b8ed8ac6b95f11
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-05 21:28:33 +0530

    Time: 149 ms (34.43%), Space: 81.1 MB (16.39%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
@@ -4,55 +4,48 @@
  * @return {number}
  */
 var shortestPath = function(grid, k) {
-    let result = Infinity;
-
-    const dx = [0, 1, -1, 0];
-    const dy = [1, 0, 0, -1];
-
     const n = grid.length;
     const m = grid[0].length;
-
-    let dist = Array.from(
-        { length: n },
-        () => Array.from(
-            { length: m },
-            () => new Array(k + 1).fill(Infinity)
-        )
-    );
-
-    function dfs(r, c, steps, remain) {
-
-        if (steps >= result)
-            return;
-
-        if (r === n - 1 && c === m - 1) {
-            result = steps;
-            return;
-        }
-
-        if (steps >= dist[r][c][remain])
-            return;
-
-        dist[r][c][remain] = steps;
-
-        for (let i = 0; i < 4; i++) {
-
-            let nr = r + dx[i];
-            let nc = c + dy[i];
-
-            if (nr < 0 || nr >= n || nc < 0 || nc >= m)
+    let visited = Array.from({length:n},()=> Array.from({length:m},()=> new Array(k+1).fill(false)));
+    let queue = [];
+    let head = 0;
+    let steps = 0;
+    const dx = [0,1,-1,0]
+    const dy = [1,0,0,-1]
+    queue.push([0,0,k]);
+    visited[0][0][k] = true;
+    while(head < queue.length)
+    {
+        let size = queue.length - head;
+        for(let i = 0;i < size ;i++)
+        {
+            const [r,c,remain] = queue[head++];
+            if(r == n-1 && c == m-1)
+            return steps
+            for(let k = 0; k < 4; k++)
+            {
+                let nr = r + dx[k];
+                let nc = c + dy[k];
+                
+                if(nr < 0 || nr >= n || nc < 0 || nc >= m)
                 continue;
+                 let newRemain = remain;
+              
+                if(grid[nr][nc] == 1)
+                {
+                  if(remain == 0)
+                  continue
+                  newRemain = remain-1 ;
+                }
+               
+                if(visited[nr][nc][newRemain])
+                continue;
+                visited[nr][nc][newRemain] = true;
 
-            if (grid[nr][nc] === 0) {
-                dfs(nr, nc, steps + 1, remain);
-            }
-            else if (grid[nr][nc] === 1 && remain > 0) {
-                dfs(nr, nc, steps + 1, remain - 1);
+                queue.push([nr,nc,newRemain]);
             }
         }
+        steps++;
     }
-
-    dfs(0, 0, 0, k);
-
-    return result === Infinity ? -1 : result;
+    return -1
 };
