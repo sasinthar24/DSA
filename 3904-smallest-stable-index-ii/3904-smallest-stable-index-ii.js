19a60518b19de3c1b8f15dc70fef06c0531ef5d3
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-05 05:39:57 +0530

    Time: 20 ms (16.67%), Space: 77.9 MB (58.33%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.js
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.js
@@ -4,22 +4,25 @@
  * @return {number}
  */
 var firstStableIndex = function(nums, k) {
-    let leftMax = new Array(nums.length).fill(0);
+   // let leftMax = new Array(nums.length).fill(0);
     let rightMin = new Array(nums.length).fill(0);
-    leftMax[0] = nums[0];
-    for(let i = 1; i < nums.length;i++)
-    {
-        leftMax[i] = Math.max(nums[i],leftMax[i-1])
-    }
+    //leftMax[0] = nums[0];
+    // for(let i = 1; i < nums.length;i++)
+    // {
+    //     leftMax[i] = Math.max(nums[i],leftMax[i-1])
+    // }
     rightMin[nums.length-1] = nums[nums.length-1]
     for(let i = nums.length-2 ; i >= 0; i--)
     {
         rightMin[i] = Math.min(nums[i],rightMin[i+1])
     }
     let ans = Infinity;
+    leftMax = nums[0]
     for(let i = 0; i < nums.length;i++)
     {
-        let val = leftMax[i] - rightMin[i]
+        if(nums[i] > leftMax)
+        leftMax = nums[i]
+        let val = leftMax - rightMin[i]
         if(val <= k)
         {
             ans = Math.min(ans,i)
