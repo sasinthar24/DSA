7d2b6244ce02b062e5d9c7d1221f2528fb1d62c9
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-08 06:34:03 +0530

    Time: 5 ms (66.67%), Space: 55.6 MB (58.33%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.js
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.js
@@ -3,25 +3,21 @@
  * @return {number}
  */
 var distinctSubseqII = function(s) {
-    const Mod = 1000000007;
+   const Mod = 1000000007;
     
-   let dp = new Array(26).fill(0);
-   for(const ch of s)
-   {
-      let total = 1;
-      for(let i = 0; i < 26; i++)
-      {
-        total = (total+dp[i]) % Mod;
-      }
-
-      let index = ch.charCodeAt(0) - 97
-      dp[index] = total;
-   }
-
-  let ans = 0;
-   for(let i = 0;i < 26 ;i++)
-   {
-    ans+=dp[i] % Mod;
-   }
-   return ans % Mod;
+    const dp = new Array(s.length+1).fill(0);
+    dp[0] = 1;
+    let map = new Map();
+    for(let i =1; i <= s.length;i++)
+    {
+       let ch = s[i-1];
+       dp[i] = (2 * dp[i-1]) %Mod;
+       if(map.has(ch))
+       {
+        let prev = map.get(ch);
+        dp[i] = (dp[i] - dp[prev-1] + Mod)% Mod
+       }
+       map.set(ch,i)
+    }
+    return (dp[s.length]-1 + Mod) % Mod
 };
