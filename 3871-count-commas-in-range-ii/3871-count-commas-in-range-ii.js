97ae0098f0c133a5948bf0bf0598d929d06ab9dc
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-09 07:01:21 +0530

    Time: 0 ms (100%), Space: 56 MB (30.77%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.js
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.js
@@ -3,19 +3,11 @@
  * @return {number}
  */
 var countCommas = function(n) {
-    if(n < 1000)
-    return 0;
-    let count =0;
-    if(n >= 1000)
-    count += Math.min(n,999999)- 1000+1;
-    if(n >= 1000000)
-    count += 2 * (Math.min(n,999999999) - 1000000 +1);
-    if(n >= 1000000000)
-    count += 3 * (Math.min(n,999999999999) - 1000000000+1);
-    if(n >= 1000000000000)
-    count += 4 * (Math.min(n,999999999999999) - 1000000000000 +1)
-    if(n >= 1000000000000000)
-    count += 5 * (n - 1000000000000000+1)
-    return count;
+    let p = 1000, res = 0;
+    while (p <= n) {
+        res += n - p + 1;
+        p *= 1000;
+    }
+    return res;
   
 };
