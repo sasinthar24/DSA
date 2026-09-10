6334edc60f7453b5ccc3f198da78235df23d4574
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-10 23:11:43 +0530

    Time: 29 ms (90.49%), Space: 56.2 MB (94.72%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
@@ -2,111 +2,32 @@
  * @param {number[][]} points
  * @return {number}
  */
- class heap 
- {
-    constructor(compare)
-    {
-        this.compare = compare;
-        this.heap = [];
-    }
 
-    size()
-    {
-        return this.heap.length;
-    }
-    isEmpty()
-    {
-        return this.heap.length == 0
-    }
-
-    peek()
-    {
-        if(this.heap.length == 0)
-        return null;
-        return this.heap[0];
-    }
-
-    push(val)
-    {
-        this.heap.push(val);
-        this.bubbleUp();
-    }
-    bubbleUp()
-    {
-        let index = this.heap.length-1;
-        while(index > 0)
-        {
-            let parent = Math.floor((index-1)/2);
-            if(this.compare(this.heap[parent],this.heap[index]))
-            break;
-            [this.heap[parent],this.heap[index]] = [this.heap[index],this.heap[parent]];
-            index = parent;
-        }
-    }
-
-    pop()
-    {
-        if(this.heap.length == 0)
-        return null;
-        if(this.heap.length == 1)
-        return this.heap.pop();
-        let top = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.bubbleDown();
-        return top;
-    }
-
-    bubbleDown()
-    {
-        let index = 0;
-        let n = this.heap.length;
-
-        while(true)
-        {
-            let left = 2 * index +1;
-            let right = 2 * index+2;
-            let best = index;
-            if(left < n && !this.compare(this.heap[best],this.heap[left]))
-            {
-                best = left;
-            }
-            if(right < n && !this.compare(this.heap[best],this.heap[right]))
-            {
-                best = right
-            }
-            if(index == best)
-            break;
-            [this.heap[best],this.heap[index]] = [this.heap[index],this.heap[best]];
-            index = best;
-        }
-    }
- }
 var minCostConnectPoints = function(points) {
-    let minHeap = new heap((a,b)=> a[0] <= b[0]);
-    const n = points.length;
-    const visited = new Array(n).fill(false);
-
-    minHeap.push([0,0]);
-    let totalCost = 0;
-    let count = 0;
-    while(count < n)
+  const n = points.length;
+  const visited = new Array(n).fill(false);
+  const minDist = new Array(n).fill(Infinity);
+  let totalCost = 0;
+  minDist[0] = 0;
+  for(let count = 0; count < n; count++)
+  {
+    let node = -1;
+    for(let i = 0; i < n;i++)
     {
-        let temp = minHeap.pop();
-        let node = temp[1];
-        if(visited[node])
-        continue;
-        visited[node] = true;
-        totalCost+=temp[0];
-        count++;
-        for(let i = 0; i < n ; i++)
-        {
-             if(visited[i])
-             continue;
-             let current = points[node];
-             let next = points[i];
-             let dis = Math.abs(current[0] - next[0]) + Math.abs(current[1] - next[1]);
-             minHeap.push([dis,i]);
-        }
+        if(!visited[i] && (node==-1 || minDist[i] < minDist[node]))
+        node = i;
     }
-    return totalCost
+
+    visited[node] = true;
+     totalCost += minDist[node];
+    for(let i = 0; i < n;i++)
+    {
+        if(visited[i])
+        continue;
+        let dis = Math.abs(points[node][0] - points[i][0])+Math.abs(points[node][1] - points[i][1])
+        minDist[i] = Math.min(minDist[i], dis )
+    }
+  }
+  return totalCost
+  
 };
