d9c4951b9ece312f846d4f74325c6c18f49bb3c8
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-11 09:30:55 +0530

    Time: 157 ms (49.02%), Space: 57.6 MB (56.86%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var findEvenNumbers = function(digits) {
+    let set = new Set();
+    for(let i = 0; i < digits.length;i++)
+    {
+        if(digits[i] == 0)
+        continue;
+        for(let j = 0; j < digits.length;j++)
+        {
+            if(i == j)
+            continue;
+            for(let k = 0; k < digits.length;k++)
+            {
+                if(i == k || j == k)
+                continue;
+                if(digits[k] % 2 != 0)
+                continue
+                let num = digits[i]*100+digits[j]*10 + digits[k]
+                set.add(num);
+            }
+        }
+    }
+    return [...set].sort((a,b)=> a -b)
+   
+};
