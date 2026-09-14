9cfe1db24366d1313e419808c28c0d922352a425
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-15 05:22:08 +0530

    Time: 258 ms (5.1%), Space: 116.4 MB (6.12%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.js
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.js
@@ -3,27 +3,48 @@
  * @return {number}
  */
 var minCut = function(s) {
-    function checkPalindrom(left,right)
-    {
-         while(left < right)
-         {
-            if(s[left] != s[right])
-            {
-                return false
-            }
-            left++;
-            right--;
-         }
-         return true;
-    }
-
+    
     const n = s.length;
+    const dp2 = Array.from({length:n},()=> new Array(n).fill(false));
+
+    for(let len = 1; len <= n; len++)
+    {
+        let i = 0;
+        let j = i + len - 1;
+        while(j < n)
+        {
+            if(len == 1)
+            {
+                dp2[i][j] = true;
+            }
+            else if(len == 2)
+            {
+                dp2[i][j] = s[i] == s[j]
+            }
+            else
+            {
+                if(s[i] == s[j])
+                {
+                    dp2[i][j] = dp2[i+1][j-1]
+                }
+                else
+                {
+                    dp2[i][j] = false;
+                }
+            }
+            i++
+            j++
+        }
+        
+    }
+   console.log(dp2)
+    
     const dp = new Array(n).fill(-1);
     function dfs(end)
     {
         if(end < 0)
         return 0
-         if(checkPalindrom(0,end))
+         if(dp2[0][end])
          return 0;
          if(dp[end] != -1)
          return dp[end];
@@ -31,7 +52,7 @@ var minCut = function(s) {
          let min = Infinity;
          for(let i = end; i>=0; i--)
          {
-            if(checkPalindrom(i,end))
+            if(dp2[i][end])
             {
                   let cutCount = 1+ dfs(i-1);
                   min = Math.min(min,cutCount);
