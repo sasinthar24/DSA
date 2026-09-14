d77e1fc260f0cbe5409b632e9ece9efb3d6ac794
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-15 04:38:04 +0530

    Time: 126 ms (69.27%), Space: 85.5 MB (42.66%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function(s) {
+    const n = s.length;
+    const dp = Array.from({length:n},()=> new Array(n).fill(-1));
+    function dfs(i,j)
+    {
+        if(i > j)
+        return 0;
+        if(i == j)
+        return 1
+        if(dp[i][j] != -1)
+        return dp[i][j];
+        
+        
+        if(s[i] == s[j])
+        {
+            dp[i][j] = 2 + dfs(i+1,j-1)
+            return dp[i][j]
+        }
+        dp[i][j] = Math.max(dfs(i+1,j),dfs(i,j-1));
+        return dp[i][j]
+    }
+    return dfs(0,n-1)
+};
