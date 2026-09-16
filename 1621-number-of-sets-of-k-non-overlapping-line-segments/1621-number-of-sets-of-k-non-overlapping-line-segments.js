77eaad5634cf14a615fe39f0aa9a6527fe376dcc
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-16 13:38:09 +0530

    Time: 131 ms (0%), Space: 91.2 MB (18.18%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.js
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.js
@@ -4,31 +4,27 @@
  * @return {number}
  */
 var numberOfSets = function(n, k) {
-   const MOD = 1000000007;
-
-    const dp = Array.from(
-        { length: n },
-        () => new Array(k + 1).fill(0)
-    );
-
-    // 0 segments
-    for (let i = 0; i < n; i++) {
+    const dp = Array.from({length:n},()=> new Array(k+1).fill(0));
+    const prefixArray = Array.from({length:n},()=> new Array(k+1).fill(0));
+    const mod = 1000000007
+    for(let i = 0;i < n ;i++)
+    {
         dp[i][0] = 1;
+        prefixArray[i][0]= i+1;
     }
-
-    for (let segments = 1; segments <= k; segments++) {
-
-        let prefix = 0;
-
-        for (let i = 1; i < n; i++) {
-
-          
-            prefix = (prefix + dp[i - 1][segments - 1]) % MOD;
-
-            dp[i][segments] =
-                (dp[i - 1][segments] + prefix) % MOD;
+    for(let segments = 1; segments <= k; segments++)
+    {
+        for(let i = 0; i < n; i++)
+        {
+              let notTake = i > 0 ? dp[i-1][segments]:0;
+              let take = i > 0 ? prefixArray[i-1][segments-1]:0;
+              dp[i][segments] =(take+notTake)% mod;
+              prefixArray[i][segments] = dp[i][segments];
+              if(i > 0)
+              {
+                prefixArray[i][segments] = (prefixArray[i-1][segments] + dp[i][segments]) % mod;
+              }
         }
     }
-
-    return dp[n - 1][k];
+    return dp[n-1][k]
 };
