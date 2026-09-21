f85eff4da0817a591e8adb15799b4e978a8602e4
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-21 21:53:32 +0530

    Time: 329 ms (32.43%), Space: 107 MB (24.32%) - LeetHub

--- 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.js
+++ 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.js
@@ -13,30 +13,34 @@ var maximumCoins = function(coins, k) {
        let sum = 0;
        for(let i = 0;  i < n;i++)
        {
-         if (j < i) { j = i; sum = 0; }  
+        //  if (j < i) { j = i; sum = 0; }  
         let start = arr[i][0];
         let end = (start + k)-1;
-        let partial = 0;
+       
         while(j < n && arr[j][1] <= end)
         {
             const [l,r,c] = arr[j];
             sum+=((r-l)+1)*c;
             j++;
         }
+         let partial = sum;
         if(j < n && arr[j][0] <= end)
         {
             const [l,r,c] = arr[j];
-            partial=((end-l)+1)*c
+            partial+=((end-l)+1)*c
         }
-         ans = Math.max(sum+partial,ans);
-            if(j > i)
-            {
-                const [l,r,c] = arr[i]
-                if(r<=end)
-                {
-                    sum-= ((r-l)+1)*c;
-                }
-            }
+        
+         ans = Math.max(partial,ans);
+            // if(j > i)
+            // {
+            //     const [l,r,c] = arr[i]
+            //     if(r<=end)
+            //     {
+            //         sum-= ((r-l)+1)*c;
+            //     }
+            // }
+              const [l,r,c] = arr[i]
+              sum-= ((r-l)+1)*c;
            
        }
         return ans;
