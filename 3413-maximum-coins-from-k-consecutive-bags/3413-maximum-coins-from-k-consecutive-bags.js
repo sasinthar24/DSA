e7b60ce1bcdfe19676e367c6d0ecda239ab4fe73
Sasinthar Elayaperumal <85315613+sasinthar24@users.noreply.github.com>
2026-09-21 21:40:53 +0530

    Time: 325 ms (37.84%), Space: 104.7 MB (29.73%) - LeetHub

---
+++ 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[][]} coins
+ * @param {number} k
+ * @return {number}
+ */
+var maximumCoins = function(coins, k) {    
+   coins.sort((a,b)=> a[0] - b[0])
+   function solve(arr)
+   {
+       let ans  = 0;
+       let j = 0;
+       const n = arr.length;
+       let sum = 0;
+       for(let i = 0;  i < n;i++)
+       {
+         if (j < i) { j = i; sum = 0; }  
+        let start = arr[i][0];
+        let end = (start + k)-1;
+        let partial = 0;
+        while(j < n && arr[j][1] <= end)
+        {
+            const [l,r,c] = arr[j];
+            sum+=((r-l)+1)*c;
+            j++;
+        }
+        if(j < n && arr[j][0] <= end)
+        {
+            const [l,r,c] = arr[j];
+            partial=((end-l)+1)*c
+        }
+         ans = Math.max(sum+partial,ans);
+            if(j > i)
+            {
+                const [l,r,c] = arr[i]
+                if(r<=end)
+                {
+                    sum-= ((r-l)+1)*c;
+                }
+            }
+           
+       }
+        return ans;
+   }
+
+   let ans = solve(coins);
+   let reversed = coins.map(([l,r,c])=>[-r,-l,c]).sort((a,b) => a[0] - b[0]);
+   ans = Math.max(ans,solve(reversed))
+   return ans;
+    
+};
